d637ded27aac296bf0f55c3a84ad9932ece24ac0
rozalia92 <d.rozalia92@gmail.com>
2014-06-26 17:54:00 +0300

    prima parte

--- lista.js
+++ lista.js
@@ -2,7 +2,24 @@
  * Created by Rozalia.Dontu on 6/26/2014.
  */
 
-angular.module('list',[])
-    .controller('listCtrl',function(){
+angular.module('lista',[])
+    .controller('listCtrl',function($scope){
 
-    })
+        $scope.checked = false;
+        $scope.newItem = {
+            pret: 0,
+            cantitate: 0,
+            nume: '',
+            descriere: ''
+        };
+
+        $scope.toggleBox = function () {
+            $scope.checked = !$scope.checked;
+        }
+
+        $scope.subtotal = function(){
+            return $scope.newItem.cantitate * $scope.newItem.pret;
+        };
+
+
+    });
