4785a7446d04d37df4c55310129afaf3998bc1d5
rozalia92 <d.rozalia92@gmail.com>
2014-06-30 16:45:58 +0300

    creat filter, suma totala

--- lista.js
+++ lista.js
@@ -8,6 +8,7 @@ angular.module('lista',[])
         $scope.shoppingList = [];
         $scope.checked = false;
         $scope.editField =false;
+        $scope.currentElem = false;
         $scope.newItem = {
             pret: 0,
             cantitate: 0,
@@ -55,6 +56,32 @@ angular.module('lista',[])
             $scope.editField = !$scope.editField;
              }
 
+        $scope.remove = function (item) {
+            for (var i = 0; i < $scope.shoppingList.length; i++){
+                if( item.nume == $scope.shoppingList[i].nume ) {
+                    $scope.shoppingList.splice(i, 1);
+                    break;
+                }
+            }
+        }
+
+        $scope.deleteCumparat = function(){
+            var old = $scope.shoppingList;
+            $scope.shoppingList = [];
+            angular.forEach(old,function(item){
+                if(!item.cumparat)
+                    $scope.shoppingList.push(item);
+            })
+        }
+
+        $scope.total = function () {
+            var tot = 0;
+            var oldV = $scope.shoppingList;
+            angular.forEach(oldV,function(item){
+                tot = tot + item.subtotal(item.pret,item.cantitate);
+            })
+            return tot;
+        }
 
 
 
